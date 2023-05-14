a010020e34bd932eab1700c55a01a89dd2a73aef
Aidan7474 <127698132+Aidan7474@users.noreply.github.com>
2023-05-15 11:38:30 +1200

    Updated

--- Fluency_Checker V1.py
+++ Fluency_Checker V1.py
@@ -1,16 +1,14 @@
-# Ask user if they can speak Maori fluently
-fluency_checker = input("Can you speak Maori fluently? : ")
+# Ask user if they can speak Maori days of the week
+fluency_checker = input("Do you know Maori days of the week? : ")
 
-# If yes print "This program is to easy for you"
+# If yes print "This program is useless to you"
 if fluency_checker == "Y":
-    print("This test is to easy for you")
+    print("This program is useless to you")
 
-# If No print "This test will help you learn Maori"
+# If No print "This program will help you learn Maori days of the week"
 elif fluency_checker == "N":
-    print("This test will help you learn Maori")
+    print("This program will help you learn Maori days of the week")
 
 # Otherwise print "Answer with Y or N"
 else:
     print("Answer with Y or N")
-
-
