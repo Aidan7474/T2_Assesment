5f4d4ecedc331762720e16e163912dce48a53385
Aidan7474 <127698132+Aidan7474@users.noreply.github.com>
2023-05-16 15:31:42 +1200

    Fluency checker V2

--- Days_of_the_week V1.py
+++ Days_of_the_week V1.py
@@ -1,1 +1,24 @@
-# Ask what the days of the week are in Maori
+# Days of the week Maori
+import random
+
+print("This is a test of your knowledge on the Maori words for the days of the week\n"
+      " ā")
+
+# List days in English
+English_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+# List days in Maori
+Maori_days = ["Rāhina", "Rātu", "Rāapa", "Rāpare", "Rāmere", "Rāhoroi", "Rātapu"]
+
+question = random.choice(English_days)
+attempt = input(f"What is the Maori name for {question}: ")
+
+# Using the index position of the question in one list to find the corresponding index position of the answer
+Answer_index = English_days.index(question)
+answer = Maori_days[Answer_index]
+
+# Check if user answer correctly
+if attempt == answer:
+    print("CORRECT")
+else:
+    print("INCORRECT")
--- Fluency_Checker V1.py
+++ Fluency_Checker V1.py
@@ -1,12 +1,12 @@
 # Ask user if they can speak Maori days of the week
-fluency_checker = input("Do you know Maori days of the week? : ")
+fluency_checker = input("Do you know Maori days of the week? : ").lower()
 
 # If yes print "This program is useless to you"
-if fluency_checker == "Y":
+if fluency_checker == "Y" or fluency_checker == "y":
     print("This program is useless to you")
 
 # If No print "This program will help you learn Maori days of the week"
-elif fluency_checker == "N":
+elif fluency_checker == "N" or fluency_checker == "n":
     print("This program will help you learn Maori days of the week")
 
 # Otherwise print "Answer with Y or N"
