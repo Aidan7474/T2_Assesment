e2e29e3798eccb69027312fa9115d702c210607f
Aidan7474 <127698132+Aidan7474@users.noreply.github.com>
2023-05-16 16:48:26 +1200

    Fluency checker V3

---
+++ Fluency_Checker V3.py
@@ -0,0 +1,25 @@
+# Functions
+def yes_no(question_text):
+    while True:
+
+        # Ask user if they can speak Maori days of the week
+        answer = input(question_text).lower()
+
+        # If yes print "This program is useless to you"
+        if answer == "Y" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If no print "This program will help you learn Maori
+        if answer == "N" or answer == "n":
+            answer = "No"
+            return answer
+
+        # Otherwise - show error
+        else:
+            print("Answer with Y or N")
+
+
+# main route
+Fluency_Checkers = yes_no("Do you know the Maori names for days of the week? ")
+
